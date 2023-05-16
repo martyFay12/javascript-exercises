c0123e91db93fbd565350d308c652939c5d78113
Marty Fay <marty.r.fay@gmail.com>
2023-05-16 16:16:22 -0300

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function (int1, int2) {
+  if (typeof int1 != "number" || typeof int2 != "number") {
+    return "ERROR";
+  }
+  if (int1 < 0 || int2 < 0) {
+    return "ERROR";
+  }
+  if (int1 === int2) {
+    return int1;
+  }
+  let sum = 0;
+  let largerInt;
+  let smallerInt;
+  if (int1 > int2) {
+    largerInt = int1;
+    smallerInt = int2;
+  } else {
+    largerInt = int2;
+    smallerInt = int1;
+  }
+  while (largerInt > smallerInt) {
+    sum += largerInt-- + smallerInt++;
+  }
+  if (largerInt === smallerInt) {
+    sum += largerInt;
+  }
+  return sum;
 };
 
 // Do not edit below this line
