0302511a70d4612b32d9773cb4e6b0c149506529
Marty Fay <marty.r.fay@gmail.com>
2023-05-16 16:02:12 -0300

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (args) {
+  arrayArgs = [...arguments];
+  let arrayToMuddle = arrayArgs[0];
+  const elementsToRemove = arrayArgs.slice(1);
+  let originalLength = arrayToMuddle.length;
+  for (element of elementsToRemove) {
+    for (let i = 0; i < originalLength; i++) {
+      if (arrayToMuddle[i] === element) {
+        originalLength--;
+        arrayToMuddle.splice(i--, 1);
+      }
+    }
+  }
+  console.log(arrayToMuddle);
+  return arrayToMuddle;
 };
 
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
