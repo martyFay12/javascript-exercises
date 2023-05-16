35fb26e3baa8e5630c33db143668cfc9cf20d533
Marty Fay <marty.r.fay@gmail.com>
2023-05-16 16:31:48 -0300

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  return typeof temp != "number"
+    ? "ERROR"
+    : Math.round(((temp - 32) * 50) / 9) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  return typeof temp != "number"
+    ? "ERROR"
+    : Math.round(((temp * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
