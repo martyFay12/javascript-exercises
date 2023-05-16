20d3378f19ee8c6bd1ff027cc36753af45f94c46
Marty Fay <marty.r.fay@gmail.com>
2023-05-16 15:34:31 -0300

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  arrayOfString = string.split("");
+  reversedArray = arrayOfString.reverse();
+  return reversedArray.join("");
 };
 
 // Do not edit below this line
