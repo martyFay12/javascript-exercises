13a3951e018184147796ec78749dc7091effa157
Marty Fay <marty.r.fay@gmail.com>
2023-05-19 11:59:22 -0300

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const punctuationless = string
+    .replace(/[.,\/#!$%\^?&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s{1,}/g, "")
+    .toLowerCase();
+  const array = punctuationless.split("");
+  const reversedArray = [...array].reverse();
+  return array.every((value, index) => value === reversedArray[index]);
 };
-
 // Do not edit below this line
 module.exports = palindromes;
