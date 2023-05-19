05d32f9b54a018e242096b38bcfa2003c103a575
Marty Fay <marty.r.fay@gmail.com>
2023-05-19 12:41:13 -0300

    Complete all exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const peopleWithAges = people.map((obj) => {
+    "yearOfDeath" in obj
+      ? (obj.age = obj.yearOfDeath - obj.yearOfBirth)
+      : (obj.age = new Date().getFullYear() - obj.yearOfBirth);
+    return obj;
+  });
+  return peopleWithAges.sort((a, b) => b.age - a.age)[0];
 };
 
 // Do not edit below this line
