12d52c68dedd558b030b409158c2d9effc4c54f8
Marty Fay <marty.r.fay@gmail.com>
2023-05-19 12:13:43 -0300

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (typeof num !== "number") num = Number(num);
+  if (num < 1) return "OOPS";
+  sequence = [1, 1];
+  let index = 0;
+  let sum;
+  while (num - index++ > 2) {
+    sum = sequence.reduce((total, current) => total + current, 0);
+    sequence[0] = sequence[1];
+    sequence[1] = sum;
+  }
+  return sequence[num - index];
 };
 
 // Do not edit below this line
